d051efaa6c93f958b3d56212425697ef8c4ccedc
Rafi Hasan <rafihasan0075@gmail.com>
2022-10-20 12:00:00 +0600

    code updated

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -20,7 +20,7 @@ const Blog = () => {
       <Accordion.Item eventKey="2">
         <Accordion.Header>What is the usage of useRef in React?</Accordion.Header>
         <Accordion.Body>
-        The useRef returns a mutable ref object. This object has a property called .current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The .current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component. 
+        The useRef returns a mutable reference object. This object has a property called .current. The value is persisted in the refContainer.current property. These values are accessed from the current property of the returned object. The .current property could be initialised to the passed argument initialValue e.g. useRef(initialValue). The object can persist a value for a full lifetime of the component. 
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
